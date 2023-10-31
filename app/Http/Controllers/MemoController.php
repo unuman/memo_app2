6a5850ebd2d543008b53a767124074468e66a905
unuman <unumakyohey@gmail.com>
2023-11-01 01:51:17 +0900

    メモアプリデータの登録まで完了

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -11,9 +11,30 @@ class MemoController extends Controller
     public function index()
     {
         // モデル名::テーブル全件取得
-        $memos =Memo::all();
+        $memos = Memo::all();
         // memosディレクトリーの中のindexページを指定し、memosの連想配列を代入
         return view('memos.index', ['memos' => $memos]);
+    }
+
+    public function create()
+    {
+        return view('memos.create');
+    }
+
+    public function store(Request $request)
+    {
+        //インスタンスの作成
+        $memo = new Memo;
+
+        // 値の用意
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        // インスタンスに値を設定して保存
+        $memo->save();
+
+        // 登録したらindexに戻る
+        return redirect(route("memos.index"));
     }
 
     // showページへ移動
